eb406943cc69c166164a9f6b293bdc869ac0cbe8
SuZuRei . <charbel.me11@gmail.com>
2025-05-20 18:16:23 -0600

    Se modifica el codigo

--- src/promedios.java
+++ src/promedios.java
@@ -1,3 +1,11 @@
-public class promedios {
+import javax.swing.JOptionPane;
 
+public class promedios{
+public static void main(String[] args) {
+    String A = JOptionPane.showInputDialog("Ingrese el primer promedio");
+    String B = JOptionPane.showInputDialog("Ingrese el segundo promedio");
+    String C = JOptionPane.showInputDialog("Ingrese el tercer promedio");
+    float total = ( Float.parseFloat(A) + Float.parseFloat(B) + Float.parseFloat(C) ) / 3;
+    System.out.println("El promedio es: " + total);
+}  
 }
